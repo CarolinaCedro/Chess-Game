ee4d1bf39591deb14f68138092f97a7c2c513d19
CarolinaCedro <yotechgirl@gmail.com>
2022-07-24 15:32:09 -0300

    feat: programation defensive

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -27,9 +27,9 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(3, 3));
-        board.placePiece(new King(board,Color.BLACK),new Position(0,4));
-        board.placePiece(new King(board,Color.WHITE),new Position(6,1));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(6, 5));
+        board.placePiece(new King(board,Color.BLACK),new Position(3,2));
+        board.placePiece(new King(board,Color.BLACK),new Position(1,6));
     }
 
 }
